018c408e36a236a21314b87ddcbf9bb581d5b287
Giacco, Angelo <giacco.angelo.s@gmail.com>
2024-04-25 19:55:10 +0200

    add response streaming

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,4 @@
 import streamlit as st
-import random
-import time
 from mistralai.client import MistralClient
 from mistralai.models.chat_completion import ChatMessage
 import os 
@@ -9,19 +7,11 @@ api_key = os.getenv("MISTRAL_API_KEY")
 if not api_key:
     if "api_key" not in st.session_state:
         st.session_state["api_key"] = st.text_input("Enter your API key", type="password")
+        api_key = st.session_state["api_key"]
     api_key = st.session_state["api_key"]
-else:
-    if expected_password := os.getenv("PASSWORD"):
-        password = st.text_input("What's the secret password?", type="password")
-        # Check if the entered key matches the expected password
-        if password != expected_password:
-            api_key = ''
-            st.error("Unauthorized access.")
-            reset_state()  # This line will reset the script
-        else:
-            api_key = os.getenv("MISTRAL_API_KEY")
 
-
+print("API KEY")
+print(api_key)
 client = MistralClient(api_key=api_key)
 
 # Initialize the model in session state if it's not already set
@@ -33,36 +23,42 @@ st.session_state["mistral_model"] = st.selectbox('Select a model', model_options
 
 st.title("Genetic Variant Analysis Bot")
 
-with st.chat_message("user"):
-    st.write("Hello 👋. I'm your personal assistant for genetic variant analysis. I have access to all the tools listed in the widely accepted framework from Richards et al 2015. Please explain the task you would like me to help with:")
-
 def response_generator():
-    response = "hi what can i do?"
-    for word in response.split():
-        yield word + " "
-        time.sleep(0.05)
+    chat_response = client.chat(
+        model=st.session_state.mistral_model,
+        messages=st.session_state.messages,
+    )
 
+    return chat_response.choices[0].message.content
+
+first_prompt = "Hello 👋. I'm your personal assistant for genetic variant analysis. I have access to all the tools listed in the widely accepted framework from Richards et al 2015. Please explain the task you would like me to help with:"
 
 if "messages" not in st.session_state:
     st.session_state.messages = []
 
+if not "system_prompt" in st.session_state:
+    st.session_state["system_prompt"] = "You are an expert in genomic medicine here to help me with my tasks in genetic variant analysis. "
+
 if st.session_state["system_prompt"] and not any(message.role == "system" for message in st.session_state.messages):
     st.session_state.messages.insert(0, ChatMessage(role="system", content=st.session_state["system_prompt"]))
+    st.session_state.messages.insert(1, ChatMessage(role="assistant", content=first_prompt))
 
 for message in st.session_state.messages:
-    if message.role != "system":  
+    if message.role != 'system':  
         with st.chat_message(message.role):
             st.markdown(message.content) 
 
-for message in st.session_state.messages:
-    with st.chat_message(message["role"]):
-        st.markdown(message["content"])
-
 if prompt := st.chat_input("Explain task..."):
-    st.session_state.messages.append({"role": "user", "content": prompt})
+    st.session_state.messages.append(ChatMessage(role="user", content=prompt))
     with st.chat_message("user"):
         st.markdown(prompt)
 
     with st.chat_message("assistant"):
-        response = st.write_stream(response_generator())
-    st.session_state.messages.append({"role": "assistant", "content": response})
+        message_placeholder = st.empty()
+        full_response = ""
+        for response in client.chat_stream(model=st.session_state.mistral_model, messages=st.session_state.messages):
+            full_response += (response.choices[0].delta.content or "")
+            message_placeholder.markdown(full_response + "|")
+        
+        message_placeholder.markdown(full_response)
+        st.session_state.messages.append(ChatMessage(role="assistant", content=full_response))
